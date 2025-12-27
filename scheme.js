c08c1cdae7876b3ce97b8b8fb24f7eedc4a6bc39
Jon Craton <jncraton@gmail.com>
2025-12-27 16:16:05 -0500

    Parse to ast

--- scheme.js
+++ scheme.js
@@ -1,4 +1,21 @@
 const scheme = {}
 
 scheme.eval = (src) => {
+  const tokens = [...src.matchAll(/(\d+|[\(\)\+\-\*\\])/g)].map(s => s[0])
+
+  const parse = (tokens, tree=[]) => {
+    let token = tokens.shift()
+
+    if (token == '(') {
+      tree.push(parse(tokens))
+      return tree
+    } else if (token == ')') {
+      return tree
+    } else {
+      tree.push(token)
+      return parse(tokens, tree)
+    }
+  }
+
+  const ast = parse(tokens)
 }

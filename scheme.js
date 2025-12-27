9a48d17dd9d318b22a5f4cc8c991ecfbaca8cb57
Jon Craton <jncraton@gmail.com>
2025-12-27 18:53:15 -0500

    Evaluate simple ast

--- scheme.js
+++ scheme.js
@@ -15,6 +15,33 @@ scheme.eval = (src) => {
     }
   }
 
+  const globalEnv = {
+    '+': (a, b) => a + b,
+  }
+
+  const eval = (ast, env=globalEnv) => {
+    if (typeof ast == 'string') {
+      return +ast
+    }
+
+    return ast.map(expr => {
+      if (Array.isArray(expr)) {
+        if (env[expr[0]]) {
+          // Found a valid name in the environment
+          if (typeof env[expr[0]] == 'function') {
+            return env[expr[0]](...expr.slice(1).map(e => eval(e, env)))
+          }
+        } else {
+          return eval(expr, env)
+        }
+        return eval(expr, Object.create(ast))
+      }
+    })
+  }
+
   const tokens = [...src.matchAll(/(\d+|[\(\)\+\-\*\\])/g)].map(s => s[0])
   const ast = parse(tokens)
+  const result = eval(ast)
+
+  return result
 }
